3c57c62c3e62fa4225b9cb0ee68d5eeee3edb320
DAMEON BRYANT <dameon@dameonbryant.com>
2015-11-27 10:22:59 -0500

    fixed a small orientation bug

--- HoodratHavoc/ViewController.m
+++ HoodratHavoc/ViewController.m
@@ -42,14 +42,14 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
+//- (NSUInteger)supportedInterfaceOrientations
+//{
+//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//        return UIInterfaceOrientationMaskAllButUpsideDown;
+//    } else {
+//        return UIInterfaceOrientationMaskAll;
+//    }
+//}
 
 - (void)didReceiveMemoryWarning
 {
